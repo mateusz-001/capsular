30f06d7680b5a07c18ecc55fb38a4300e176273a
Mateusz Stopa <matistopa2001@gmail.com>
2026-07-27 13:47:48 +0200

    feat/backend: outfits tags CRUD

--- backend/src/controllers/universal.controller.ts
+++ backend/src/controllers/universal.controller.ts
@@ -1,5 +1,13 @@
 import type { Request, Response } from 'express';
-import { getColors, getMaterials } from '../services/universal.service.js';
+import {
+  createTag,
+  editTag,
+  getColors,
+  getMaterials,
+  getTags,
+  removeTag,
+} from '../services/universal.service.js';
+import { itemIdSchema } from '../schemas/universal.schema.js';
 
 export const getAvailableColors = async (_req: Request, res: Response) => {
   const colors = await getColors();
@@ -11,4 +19,40 @@ export const getAvailableMaterials = async (_req: Request, res: Response) => {
   const materials = await getMaterials();
 
   res.status(200).json({ data: materials });
+};
+
+export const getAvailableTags = async (_req: Request, res: Response) => {
+  const tags = await getTags();
+
+  res.status(200).json({ data: tags });
+};
+
+export const addTag = async (req: Request, res: Response) => {
+  const { name } = req.body;
+  const userId = req.user.userId;
+
+  const tag = await createTag({ userId, name });
+
+  res.status(200).json({ message: 'Tag created successfully', data: tag });
+};
+
+export const deleteTag = async (req: Request, res: Response) => {
+  const tagId = itemIdSchema.parse(req.params.tagId);
+  const userId = req.user.userId;
+
+  await removeTag(userId, tagId);
+
+  res.sendStatus(204);
+};
+
+export const updateTag = async (req: Request, res: Response) => {
+  const tagId = itemIdSchema.parse(req.params.tagId);
+  const userId = req.user.userId;
+  const { name } = req.body;
+
+  const updatedTag = await editTag({ userId, tagId, name });
+
+  res
+    .status(200)
+    .json({ message: 'Tag updated successfully', data: updatedTag });
 };
--- backend/src/routes/universal.routes.ts
+++ backend/src/routes/universal.routes.ts
@@ -2,13 +2,21 @@ import { Router } from 'express';
 import { authMiddleware } from '../middlewares/auth.middleware.js';
 import { asyncHandler } from '../utils/asyncHandler.js';
 import {
+  addTag,
+  deleteTag,
   getAvailableColors,
   getAvailableMaterials,
+  getAvailableTags,
+  updateTag,
 } from '../controllers/universal.controller.js';
 
 const router = Router();
 
 router.get('/colors', authMiddleware, asyncHandler(getAvailableColors));
 router.get('/materials', authMiddleware, asyncHandler(getAvailableMaterials));
+router.get('/tags', authMiddleware, asyncHandler(getAvailableTags));
+router.post('/tags', authMiddleware, asyncHandler(addTag));
+router.delete('/tags/:tagId', authMiddleware, asyncHandler(deleteTag));
+router.patch('/tags/:tagId', authMiddleware, asyncHandler(updateTag));
 
 export default router;
